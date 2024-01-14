300087383af15627264ad35f17fad463cb4cf1e3
Derik Christian <SDerikCh@hotmail.com>
2024-01-14 19:41:05 -0300

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("9999-9999", "Nokia NK47", "7H8FH29GAH", 128);
+Iphone i1 = new Iphone("9999-9961", "Steve Jobs 2", "LFSH79479", 2048);
+
+Console.WriteLine("Testes Nokia");
+n1.Ligar();
+n1.InstalarAplicativo("GOOGLE Maps");
+n1.ReceberLigacao();
+Console.WriteLine(n1.Numero);
+Console.WriteLine();
+Console.WriteLine("Testes Iphone");
+i1.Ligar();
+i1.InstalarAplicativo("Angry birds Steve Jobs Version");
+i1.ReceberLigacao();
+Console.WriteLine(i1.Numero);
